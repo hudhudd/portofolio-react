11938463a02cc34ce96c2f5a4160a510a0b7cf80
hudhudd <yehudaor987@gmail.com>
2022-09-03 14:58:16 +0700

    Porto 1.0 React
    Navbar and Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import AppShellDemo from "./component/navbar/appshell";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppShellDemo />
+      <h1>test</h1>
     </div>
   );
 }
